ded7c508a75d40a009c72076a3c7e1ec51d67de2
Markusandrew <123773526+Markusandrew@users.noreply.github.com>
2023-12-15 19:38:47 -0800

    Add files via upload

---
+++ JS/backtotop.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// scroll to the top when clicked
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
